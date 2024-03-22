d675def230c342fa29cb1b64c07fef6e6b2281a9
Yuri Alves Clemente <116123245+yclemente1357@users.noreply.github.com>
2024-03-22 15:56:05 -0300

    Subindo projeto

---
+++ ControleDeContatos/ControleDeContatos/Program.cs
@@ -0,0 +1,43 @@
+using ControleDeContatos.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+
+namespace ControleDeContatos;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddControllersWithViews();
+
+        builder.Services.AddDbContext<BancoContext>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("MinhaConexaoSQL")));
+
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Home/Error");
+        }
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+
+        app.Run();
+
+
+    }
+
+    
+}
